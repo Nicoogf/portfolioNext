e9f3cdd593a4bd7bcd1844a9110c2b0fd32003ca
Nicoogf <nicolasgfalabella@gmail.com>
2023-11-09 14:36:07 -0300

    Sin config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
-  distDir: 'out',
-  basePath: '/portfolioNext',
-  output: 'export',
-  experimental: {
-    serverActions: true, 
-  }
+
 }
 
 module.exports = nextConfig;
